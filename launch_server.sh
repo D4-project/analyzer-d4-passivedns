d85e4422c996ee45bdbb595e66bffce0e7018f57
Jean-Louis Huynen <huynenjl@gmail.com>
2019-05-28 07:49:37 +0200

    fix: [launcher] cd in subshell

--- launch_server.sh
+++ launch_server.sh
@@ -12,8 +12,8 @@ fi
 screen -dmS "pdns"
 sleep 0.1
 
-screen -S "pdns" -X screen -t "pdns-lookup-redis" bash -c "(/home/d4/analyzer-d4-passivedns/redis/src/redis-server /home/d4/analyzer-d4-passivedns/etc/redis.conf; read x;)"
-screen -S "pdns" -X screen -t "pdns-ingester" bash -c ${ENV_PY}' ./bin/pdns-ingestion.py;'
-screen -S "pdns" -X screen -t "pdns-cof" bash -c ${ENV_PY}' ./bin/pdns-cof-server.py;'
+screen -S "pdns" -X screen -t "pdns-lookup-redis" bash -c "(/home/d4/analyzer-d4-passivedns/redis/src/redis-server /home/d4/analyzer-d4-passivedns/etc/redis.conf); read x;"
+screen -S "pdns" -X screen -t "pdns-cof" bash -c "(cd bin; ${ENV_PY} ./pdns-cof-server.py; read x;)"
+screen -S "pdns" -X screen -t "pdns-ingester" bash -c "(cd bin; ${ENV_PY} ./pdns-ingestion.py; read x;)"
 
 exit 0
